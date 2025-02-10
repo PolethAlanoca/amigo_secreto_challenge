749f8689fd70ebe2d60a88e73329de65d508c8ca
PolethAlanoca <katy_girl_17@hotmail.com>
2025-02-09 22:58:47 -0500

    Segundo Avance

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo (amigo){
+    let amigo = document.getElementById("amigo");
+    amigos.push(amigo);
+    asignarTextoElemento("ul", amigos);
+}
+
+function asignarTextoElemento(elemento, lista) {
+    let elementoHTML = document.getElementById(elemento);
+    elementoHTML.innerHTML = lista;
+    return;
+}
