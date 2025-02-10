3d1f2feb86915342faca6e30478f689301f70f4b
PolethAlanoca <katy_girl_17@hotmail.com>
2025-02-10 16:16:33 -0500

    tercer cambio

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
 
-function agregarAmigo (amigo){
-    let amigo = document.getElementById("amigo");
+function agregarAmigo (){
+    let input = document.getElementById("amigo");
+    let amigo = input.value.trim();
+    if (amigo == "") {
+        alert("Por favor, ingresa un nombre válido.");
+        return;
+    }
     amigos.push(amigo);
-    asignarTextoElemento("ul", amigos);
+    asignarTextoElemento();
 }
 
-function asignarTextoElemento(elemento, lista) {
-    let elementoHTML = document.getElementById(elemento);
-    elementoHTML.innerHTML = lista;
-    return;
+function asignarTextoElemento() {
+    let elementoHTML = document.getElementById("listaAmigos");
+    elementoHTML.innerHTML = "";
+    amigos.forEach(elemento => {
+        let li = document.createElement("li");
+        li.textContent = nombre;
+        lista.appendChild(li);
+    });       
 }
